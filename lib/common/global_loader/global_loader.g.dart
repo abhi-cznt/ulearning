5b76d8b893a0f3833c1a1a2f03c4a09e3517247a
Sushil <Sushilsingh5300@gmail.com>
2023-10-16 18:13:44 +0530

    16 Oct reached to the backend at part three

---
+++ lib/common/global_loader/global_loader.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'global_loader.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$appLoaderHash() => r'b2c52cbf3edc6e8bbbc679c083669b69abbda3b2';
+
+/// See also [AppLoader].
+@ProviderFor(AppLoader)
+final appLoaderProvider = AutoDisposeNotifierProvider<AppLoader, bool>.internal(
+  AppLoader.new,
+  name: r'appLoaderProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$appLoaderHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AppLoader = AutoDisposeNotifier<bool>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
---
+++ lib/features/home/controller/home_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'home_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$homeScreenDotsBannerHash() =>
+    r'655d2da08a282a9b5bd325e35f0c72def7d1869b';
+
+/// See also [HomeScreenDotsBanner].
+@ProviderFor(HomeScreenDotsBanner)
+final homeScreenDotsBannerProvider =
+    NotifierProvider<HomeScreenDotsBanner, int>.internal(
+  HomeScreenDotsBanner.new,
+  name: r'homeScreenDotsBannerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$homeScreenDotsBannerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$HomeScreenDotsBanner = Notifier<int>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
